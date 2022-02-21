af5fe1ebe47924004769ac35f85828b56f011540
Patrícia Souza <patricia.sz.br@gmail.com>
2022-02-20 22:12:24 -0300

    criação e estilização do form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,19 +1,48 @@
 import Modal from 'react-modal';
+import { Container } from './style';
 
+import closeImg from '../../assets/close.svg'
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 
 }
 
-export function NewTransactionModal({isOpen,onRequestClose}:NewTransactionModalProps){
-    return(
-      <Modal 
-        isOpen={isOpen}
-        onRequestClose={onRequestClose}
-      >
+export function NewTransactionModal({ isOpen,onRequestClose }: NewTransactionModalProps){
+  return (
+    <Modal 
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button 
+        type="button"
+        onClick={onRequestClose} 
+        className="react-modal-close"
+        >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+      <Container>
         <h2>Cadastrar Transação</h2>
-      </Modal>
-    
-    )
+
+        <input
+          placeholder="T[itulo"
+        />
+
+        <input 
+          type="number" 
+          placeholder="Valor"
+        />
+        <input 
+          placeholder="Categoria"
+        />
+        
+        <button type="submit">
+          Cadastrar
+        </button>
+
+      </Container>
+    </Modal>
+  );
 }
